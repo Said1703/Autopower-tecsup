2d23eb4fcb2b433f51a36f870ff9573f2e90b3bc
Said1703 <said-drk@hotmail.com>
2023-10-08 10:39:20 -0500

    seteo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,14 +14,14 @@ export default function Home() {
     <>
       <main className="bg-white-skyblue">
         <Layout />
-        <section>
-          <Hero />
-        </section>
-        <section class="mt-6 md:mt-24 w-[70%] m-auto">
-          <h2 class="text-lg lg:text-4xl text-center my-3 font-bold tracking-wider ">
+
+        <Hero />
+
+        <section className="mt-6 md:mt-24 w-[70%] m-auto">
+          <h2 className="text-lg lg:text-4xl text-center my-3 font-bold tracking-wider ">
             Conoce nuestros planes de seguro vehicular
           </h2>
-          <div class="md:flex mt-8 md:mt-16 gap-12 ">
+          <div className="md:flex mt-8 md:mt-16 gap-12 ">
             <ContainerPriceBasic />
             <ContainerPriceGold />
             <ContainerPricePlatinum />
