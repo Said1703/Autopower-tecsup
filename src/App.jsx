3fa5ed0b4a5e8b9ae6ca3e95f929fc372118f9f2
Said1703 <said-drk@hotmail.com>
2023-10-07 19:21:45 -0500

    seteo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Router from "./router";
 import { Provider } from "react-redux";
-import { store } from "./store";
 export default function App() {
   return (
-    <Provider store={store}>
+    <Provider>
       <Router />
     </Provider>
   );
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import autoPowerSlice from "../slices/autoPowerSlice";
-export const store = configureStore ({
-    reducer:{
-        autopower: autoPowerSlice
-    },
-}
-
-);
