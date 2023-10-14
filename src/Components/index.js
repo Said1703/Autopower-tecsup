a48a0e0cebc37f3b2811d94ae339db5d05c0aef7
Said1703 <said-drk@hotmail.com>
2023-10-14 16:31:37 -0500

    agregando containerform

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,6 +4,7 @@ export {default as CircleButton} from "./CircleButton";
 export {default as ContainerPriceBasic} from "./ContainerPriceBasic";
 export {default as ContainerPriceGold} from "./ContainerPriceGold";
 export {default as ContainerPricePlatinum} from "./ContainerPricePlatinum";
+export {default as ContainerForm} from "./ContainerForm";
 export {default as Layout} from "./Layout";
 export {default as Hero} from "./Hero";
 export {default as ProgressBar} from "./ProgressBar";
--- src/pages/BuyService/index.jsx
+++ src/pages/BuyService/index.jsx
@@ -1,9 +1,10 @@
 import { Layout } from "../../Components";
+import { ContainerForm } from "../../Components";
 export default function BuyService() {
   return (
     <>
       <main className="bg-white-skyblue">
-        <Layout />
+        <ContainerForm />
       </main>
     </>
   );
