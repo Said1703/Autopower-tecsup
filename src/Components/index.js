a34c872c3e55f96073ac3927d3d5a0c3fe418f40
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2023-11-03 21:48:01 -0500

    detalle2

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,6 @@
 export {default as Button} from "./Button";
 export {default as CircleButton} from "./CircleButton";
-export {default as ContainerPayment} from "./ContainerPayment"
+export {default as ContainerPayment} from "./ContainerPayment";
 export {default as ContainerPriceBasic} from "./ContainerPriceBasic";
 export {default as ContainerPriceGold} from "./ContainerPriceGold";
 export {default as ContainerPricePlatinum} from "./ContainerPricePlatinum";
