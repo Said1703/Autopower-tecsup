e0d4a382445d95e4a0ae084558536bc712fe44b7
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2023-11-04 11:49:25 -0500

    finalizado 2.0

--- src/Components/HeroBenefit/index.jsx
+++ src/Components/HeroBenefit/index.jsx
@@ -1,6 +1,6 @@
 import beneficios from "../../../public/beneficios_web.jpg";
 export default function HeroBenefit() {
   return (
-    <img className="w-full h-96 bg-cover bg-center" src={beneficios} alt="" />
+    <img className="md:w-full md:h-96 bg-cover bg-center" src={beneficios} alt="" />
   );
 }
