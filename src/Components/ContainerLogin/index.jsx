884bb32928846dfce5647f07e06d9b9d2b3b2e72
Said1703 <said-drk@hotmail.com>
2024-01-20 14:07:40 -0500

    cambio de nombres

--- src/Components/ContainerLogin/index.jsx
+++ src/Components/ContainerLogin/index.jsx
@@ -1,106 +1,96 @@
 /* eslint-disable react/prop-types */
-import TextFaild from '../TextField';
-import FormWindow from '../FormWindow'
-import Button from '../Button';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-
+import TextFaild from "../TextField";
+import FormWindow from "../FormWindow";
+import Button from "../Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ContainerLogin() {
+  const [values, setValues] = useState({
+    placa_vehiculo: "",
+    tipo_documento: "",
+    numeroDoc: "",
+  });
 
-    const [values, setValues] = useState({
-        placa: '',
-        tipoDocumento: '',
-        numeroDoc: '',
-    })
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [buttonActive, setButtonActive] = useState(false);
 
-    const [buttonActive, setButtonActive] = useState(false)
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
 
-    const handleInputChange = (evt) => {
-        const { name, value } = evt.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
 
-        setValues((prev) => ({ ...prev, [name]: value }));
+    const inputs = Object.values(values);
 
-        const inputs = Object.values(values)
+    const complete = inputs.every((input) => input !== "");
 
-        const complete = inputs.every(input => input !== "")
-
-        if (complete) {
-            setButtonActive(true)
-        }
-        
+    if (complete) {
+      setButtonActive(true);
     }
+  };
 
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-        navigate("/profile")
-        console.log(values);
-      };
-    
-    return (
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    navigate("/profile");
+    console.log(values);
+  };
 
-        <FormWindow className={"md:h-[93vh] pt-20"}>
+  return (
+    <FormWindow className={"md:h-[93vh] pt-20"}>
+      <form onSubmit={handleFormSubmit}>
+        <h2 className="text-2xl font-bold text-center mt-2 mb-4 text-blue-900">
+          Consulta Autopower
+        </h2>
+        <hr className="bg-blue-400" />
 
-            <form onSubmit={handleFormSubmit}>
+        <div className="py-2">
+          <TextFaild
+            type="text"
+            name="placa"
+            placeholder="Placa"
+            className="w-full"
+            onChange={handleInputChange}
+            required
+          />
+        </div>
 
-                <h2 className="text-2xl font-bold text-center mt-2 mb-4 text-blue-900">Consulta Autopower</h2>
-                <hr className="bg-blue-400" />
+        <div className="py-2">
+          <select
+            type="text"
+            name="tipo_documento"
+            placeholder="Tipo de documento"
+            className="border-b-2 w-full px-3 py-2  focus:outline-none focus:border-blue-500 bg-white text-gray-400"
+            onChange={handleInputChange}
+          >
+            <option value="0">Tipo de documento</option>
+            <option value="ced">Cedula</option>
+            <option value="tar">Tarjeta</option>
+            <option value="dni">DNI</option>
+          </select>
+        </div>
 
-                <div className="py-2">
-                    <TextFaild
-                    type="text"
-                    name="placa"
-                    placeholder="Placa"
-                    className="w-full"
-                    onChange={handleInputChange}
-                    required
-                    />
-                </div>
+        <div className="py-2">
+          <TextFaild
+            type="number"
+            name="numeroDoc"
+            placeholder="Numero de documento"
+            className="w-full"
+            onChange={handleInputChange}
+            required
+          />
+        </div>
 
-                <div className="py-2">
-                    <select
-                    type="text"
-                    name="tipoDocumento"
-                    placeholder="Tipo de documento"
-                    className="border-b-2 w-full px-3 py-2  focus:outline-none focus:border-blue-500 bg-white text-gray-400"
-                    onChange={handleInputChange}
-                    >
-                    <option value="0">Tipo de documento</option>
-                    <option value="ced">Cedula</option>
-                    <option value="tar">Tarjeta</option>
-                    <option value="dni">DNI</option>
-                    </select>            
-                </div>
-
-                <div className="py-2">
-                    <TextFaild
-                    type="number"
-                    name="numeroDoc"
-                    placeholder="Numero de documento"
-                    className="w-full"
-                    onChange={handleInputChange}
-                    required
-                    />
-                </div>
-
-                <div className="text-center pt-6">
-                    <Button
-                    type="submit"
-                    text="Buscar"
-                    variant="primary"
-                    disabled={!buttonActive}
-                    onChange = {handleInputChange}
-                    />
-                </div>
-
-            </form>
-          
-            
-
-        </FormWindow>
-
-    );
+        <div className="text-center pt-6">
+          <Button
+            type="submit"
+            text="Buscar"
+            variant="primary"
+            disabled={!buttonActive}
+            onChange={handleInputChange}
+          />
+        </div>
+      </form>
+    </FormWindow>
+  );
 }
